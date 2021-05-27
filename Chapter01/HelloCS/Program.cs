1e4bc93f240ae3298d0263c1e2d5b9e8ea563e07
Herman Cruz <amnher@gmail.com>
2021-05-27 18:01:13 +0800

    Chapter2

--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
+using System.Linq;
+using System.Reflection;
+// #error version
 namespace HelloCS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int z;
+            double heightInMetres = 1.88;
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+           
             Console.WriteLine("Hello, C#!");
         }
     }
